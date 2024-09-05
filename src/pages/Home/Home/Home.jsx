5f61b9d4c42d72cfd25ebf37e7bb1163aa5a24b1
aticcmahbub <aticcmahbub@gmail.com>
2024-09-05 22:52:09 +0600

    footer added git push

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,14 +1,6 @@
-import { useState } from "react";
 
 const Home = () => {
-    const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-      fetch("https://endgame-job-task-server.vercel.app/api/products")
-        .then((res) => res.json())
-        .then(data => setProducts(products));
-    }, [products]);
-    console.log(products);
     return (
         <div>
             Home
